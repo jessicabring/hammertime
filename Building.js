b72aa330ad412d35ef9986016821493a5d759629
Madeleine Genova <madiane@unc.edu>
2022-02-19 18:36:32 -0500

    building class

---
+++ Building.js
@@ -0,0 +1,12 @@
+class Building {
+    constructor(lat, long, distance, type, price, social, bed, bath) {
+        this.lat = lat;
+        this.long = long;
+        this.distance = distance;
+        this.type = type;
+        this.price = price;
+        this.social = social;
+        this.bed = bed;
+        this.bath = bath;
+    }
+}
