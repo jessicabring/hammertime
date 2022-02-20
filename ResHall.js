59a8c8fbb76056072b005bcb5dbd822b66bff85c
Madeleine Genova <madiane@unc.edu>
2022-02-19 19:29:08 -0500

    building & reshall classes

---
+++ ResHall.js
@@ -0,0 +1,12 @@
+
+
+class ResHall {
+    constructor(name, lat, long, distance, social, style) {
+        this.name = name;
+        this.lat = lat;
+        this.long = long;
+        this.distance = distance;
+        this.social = social;
+        this.style = style;
+    }
+}
---
+++ reshalls.js
@@ -0,0 +1,3 @@
+var alderman = new ResHall(
+    "Alderman",35.9149,-79.04816,1,2,corridor
+)
