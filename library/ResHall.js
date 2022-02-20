9ee7998a5a7a256fa495687a566effdc29d6abee
Madeleine Genova <madiane@unc.edu>
2022-02-19 20:32:55 -0500

    added all residence halls

--- library/ResHall.js
+++ library/ResHall.js
@@ -1,10 +1,11 @@
 
 
 class ResHall {
-    constructor(name, lat, long, distance, social, style) {
+    constructor(name, lat, long, laundry, social, style) {
         this.name = name;
         this.lat = lat;
         this.long = long;
+        this.laundry = laundry;
         this.social = social;
         this.style = style;
     }
